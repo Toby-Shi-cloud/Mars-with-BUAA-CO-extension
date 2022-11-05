a57c17f33c8e715debe33fb82189f05a3fbee7d7
Toby Shi <toby3030@qq.com>
2022-11-05 13:37:48 +0800

    将测试移动到测试文件夹

--- cleanAll.bat
+++ cleanAll.bat
@@ -1,3 +1,5 @@
 
 del /a /f /s /q *.class
 del srcList.txt
+del test\testcode.txt
+del test\srcList.txt
---
+++ test/compileClass.bat
@@ -0,0 +1,3 @@
+@echo off
+
+javac -encoding gbk test\adduo.java test\subuo.java test\bhelbal.java
